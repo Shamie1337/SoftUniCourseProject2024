af4d405bfb43a08e4556078b394363f58f97e317
Nikola Shalamanov <nikolashalamanov999@gmail.com>
2024-03-05 13:36:51 +0200

    Added ServiceCollection class

---
+++ SoftUniCourseProject2024/Extensions/ServiceCollectionExtention.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SoftUniCourseProject2024.Data;
+
+namespace SoftUniCourseProject2024.Extensions
+{
+    static class ServiceCollectionExtention
+    {
+        static public IServiceCollection AddApplicationServices(this IServiceCollection services)
+
+        {
+            return services;
+        }
+
+
+        static public IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        static public IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+
+        {
+
+            services
+                .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
